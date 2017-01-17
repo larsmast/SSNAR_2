bdea9ee0743e6432796c09057d25242eac4c62dc
geirhei <geirhei@stud.ntnu.no>
2017-01-17 18:03:35 +0100

    add SlamMappingController

--- SSNAR/src/no/ntnu/et/simulator/SlamRobot.java
+++ SSNAR/src/no/ntnu/et/simulator/SlamRobot.java
@@ -16,13 +16,33 @@ import no.ntnu.et.general.Pose;
  * @author geirhei
  */
 public class SlamRobot extends SimRobot {
+    private final int windowHeight = 100;
+    private final int windowWidth = 100;
     private int[][] mapWindow;
     private LinkedBlockingQueue<int[]> measurementQueue;
     
-    public SlamRobot(SimWorld world, Pose initialPose, String name, int id) {
+    SlamRobot(SimWorld world, Pose initialPose, String name, int id) {
         super(world, initialPose, name, id);
-        mapWindow = new int[100][100];
+        mapWindow = initMapWindow(windowHeight, windowWidth);
         measurementQueue = new LinkedBlockingQueue(5);
+    }
+    
+    private int[][] initMapWindow(int height, int width) {
+        int[][] window = new int[height][width];
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                window[i][j] = 2;
+            }
+        }
+        return window;
+    }
+    
+    public int[][] getMapWindow() {
+        return mapWindow;
+    }
+    
+    public LinkedBlockingQueue<int[]> getMeasurementQueue() {
+        return measurementQueue;
     }
     
     /**
--- SSNAR/src/no/ntnu/ge/slam/SlamMappingController.java
+++ SSNAR/src/no/ntnu/ge/slam/SlamMappingController.java
@@ -6,10 +6,73 @@
  */
 package no.ntnu.ge.slam;
 
+import java.util.concurrent.LinkedBlockingQueue;
+import no.ntnu.et.simulator.SlamRobot;
+import no.ntnu.tem.communication.Inbox;
+
 /**
  *
  * @author geirhei
  */
-public class SlamMappingController {
+public class SlamMappingController extends Thread {
+    private final int cellSize = 2;
+    private SlamRobot robot;
+    private Inbox inbox;
+    private int[][] mapWindow;
+    private boolean paused;
+    private LinkedBlockingQueue<int[]> measurementQueue;
+    private int[] currentMeasurement;
     
+    public SlamMappingController(SlamRobot robot, Inbox inbox) {
+        this.robot = robot;
+        this.inbox = inbox;
+        mapWindow = robot.getMapWindow();
+        measurementQueue = robot.getMeasurementQueue();
+        currentMeasurement = new int[20];
+    }
+    
+    @Override
+    public void start(){
+        if(!isAlive()){
+            super.start();
+        }
+        paused = false;
+    }
+
+    /**
+     * Pauses the mapping
+     */
+    public void pause(){
+        paused = true;
+    }
+    
+    /**
+     * Returns if the mapping is running or paused
+     * @return 
+     */
+    public boolean isRunning(){
+        return !paused;
+    }
+    
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                break;
+            }
+            if (paused){
+                continue;
+            }
+            
+            currentMeasurement = measurementQueue.poll();
+            if (currentMeasurement == null) {
+                System.out.println("currentMeasurement = null");
+                continue;
+            }
+            
+        }
+    }
 }
