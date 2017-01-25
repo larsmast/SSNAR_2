eae8c1c91d5ca1e09602df38fd92830973931956
geirhei <geirhei@stud.ntnu.no>
2017-01-25 17:25:24 +0100

    constructor

--- SSNAR/src/no/ntnu/ge/slam/SlamMeasurementHandler.java
+++ SSNAR/src/no/ntnu/ge/slam/SlamMeasurementHandler.java
@@ -6,10 +6,33 @@
  */
 package no.ntnu.ge.slam;
 
+import no.ntnu.et.general.Pose;
+import no.ntnu.et.mapping.Sensor;
+import no.ntnu.et.simulator.SlamRobot;
+import no.ntnu.tem.robot.Measurement;
+
 /**
  *
  * @author geirhei
  */
 public class SlamMeasurementHandler {
+    private final Pose initialPose;
+    private final int sensorRange = 40;
+    private Measurement currentMeasurement;
+    private Pose robotPose;
+    private Sensor[] sensors;
+    private SlamRobot robot;
     
+    public SlamMeasurementHandler(SlamRobot robot, Pose initialPose) {
+        this.initialPose = initialPose;
+        this.robot = robot;
+        sensors = new Sensor[4];
+        for (int i = 0; i < 4; i++) {
+            sensors[i] = new Sensor();
+        }
+    }
+    
+    boolean updateMeasurement() {
+        
+    }
 }
